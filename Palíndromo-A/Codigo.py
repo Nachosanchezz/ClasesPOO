5aee42980ced827ab3daff846529c71efe58aebc
Alejandro Serrano Catalina <a.serranocatalina@gmail.com>
2023-03-28 19:08:11 +0200

    definimos la clase de palindromo

---
+++ Palíndromo-A/Codigo.py
@@ -0,0 +1,21 @@
+class palindromoA
+    def __init__(self, frase):
+        self.frase = frase
+        self.frase = self.frase.replace(" ", "")
+        self.frase = self.frase.lower()
+        self.frase = self.frase.replace("á", "a")
+        self.frase = self.frase.replace("é", "e")
+        self.frase = self.frase.replace("í", "i")
+        self.frase = self.frase.replace("ó", "o")
+        self.frase = self.frase.replace("ú", "u")
+        self.frase = self.frase.replace("ñ", "n")
+        self.frase = self.frase.replace("ü", "u")
+        self.frase = self.frase.replace("¿", "")
+        self.frase = self.frase.replace("?", "")
+        self.frase = self.frase.replace("¡", "")
+        self.frase = self.frase.replace("!", "")
+        self.frase = self.frase.replace(",", "")
+        self.frase = self.frase.replace(".", "")
+        self.frase = self.frase.replace(":", "")
+        self.frase = self.frase.replace(";", "")
+        self.frase = self.frase.replace("(", "")
